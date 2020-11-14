a31afdca092125f6a43ea0c10d66124827f58cea
mckaysmith <mckaysmith11@gmail.com>
2020-11-14 15:33:04 -0700

    guessing game

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,52 @@
 fun main(args : Array<String>) {
-    println("Hello, World!")
+    val list = arrayListOf<Int>()
+
+    for (x in 1 until 101) {
+        list.add(x)
+    }
+
+    var guessed = false
+
+    var numToGuess = -1
+
+    var stringInput : String
+
+    while (!guessed)
+    {
+        if (numToGuess == -1)
+        {
+            numToGuess = list.random()
+        }
+
+        print("Pick a number between 1 and 100:   ")
+
+        stringInput = readLine()!!
+
+
+        if (stringInput.toInt() == numToGuess)
+        {
+            print("Corret! Play again? [y/n]:   ")
+
+            stringInput = readLine()!!
+
+            if (stringInput.equals("y"))
+            {
+                numToGuess = -1
+            }
+            else
+            {
+                guessed = true
+            }
+        }
+        else if (stringInput.toInt() > numToGuess)
+        {
+            println("Too high! Try again!")
+        }
+
+        else if (stringInput.toInt() < numToGuess)
+        {
+            println("Too low! Try again!")
+        }
+    }
+
 }
